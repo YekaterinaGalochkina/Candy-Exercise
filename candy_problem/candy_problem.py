a4cc4f1508b52cfc6460d42b886c9da99d7c892f
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-03-18 18:18:45 -0700

    Implementing 4 functions/ tests

--- candy_problem/candy_problem.py
+++ candy_problem/candy_problem.py
@@ -1,13 +1,60 @@
-def get_friends_favorite_candy_count(favorites):
-    pass
 
-def create_new_candy_data_structure(data):
-    pass 
+friend_favorites = [
+    ["Sally", ["lollipop", "bubble gum", "laffy taffy" ]],
+    ["Bob", ["milky way", "licorice", "lollipop" ]],
+    ["Arlene", ["chocolate bar", "milky way", "laffy taffy" ]],
+    ["Carlie", ["nerds", "sour patch kids", "laffy taffy" ]]
+]
 
-def get_friends_who_like_specific_candy(data, candy_name):
-    pass
+def get_friends_favorite_candy_count(friend_favorites):
+    candy_count = {}
+    for lists in friend_favorites:
+        for items in lists[1]:
+            if items in candy_count:
+                candy_count[items] += 1
+            else:
+                candy_count[items] = 1
+    return candy_count
+#print(get_friends_favorite_candy_count(friend_favorites))
 
-def create_candy_set(data):
-    pass 
+
+def create_new_candy_data_structure(friend_favorites):
+    candy_dict = {}  
+    
+    for friend, candies in friend_favorites:
+        for candy in candies:
+            if candy not in candy_dict:
+                candy_dict[candy] = []  
+            candy_dict[candy].append(friend)  
+    
+    return candy_dict
+# print(create_new_candy_data_structure(friend_favorites))
+
+def get_friends_who_like_specified_candy(friend_favorites, candy_name):
+    list = []
+    
+    for friend, candies in friend_favorites:
+        
+        if candy_name in candies:
+            list.append(friend)
+    return tuple(list)
+# print(get_friends_who_like_specified_candy(friend_favorites, "lollipop"))
 
 
+candy_dict = {
+    'lollipop': ['Sally', 'Bob'], 
+    'bubble gum': ['Sally'], 
+    'laffy taffy': ['Sally', 'Arlene', 'Carlie'], 
+    'milky way': ['Bob', 'Arlene'], 
+    'licorice': ['Bob'], 
+    'chocolate bar': ['Arlene'], 
+    'nerds': ['Carlie'], 
+    'sour patch kids': ['Carlie']
+    }
+
+def create_candy_set(candy_dict):
+    candy_set = set()
+    for keys in candy_dict.keys():
+        candy_set.add(keys)
+    return candy_set
+# print(create_candy_set(candy_dict))
--- tests/test_candy_data_structure.py
+++ tests/test_candy_data_structure.py
@@ -17,6 +17,41 @@ def test_create_candy_data_structure_type():
     # Assert
     assert type(new_candy_data) == dict
 
+
+def test_candy_count_correctness():
+    friend_favorites = [
+        ["Sally", ["lollipop", "bubble gum", "laffy taffy"]],
+        ["Bob", ["milky way", "licorice", "lollipop"]],
+        ["Arlene", ["chocolate bar", "milky way", "laffy taffy"]],
+        ["Carlie", ["nerds", "sour patch kids", "laffy taffy"]]
+    ]
+
+    new_candy_data = get_friends_favorite_candy_count(friend_favorites)
+
+    expected_output = {
+        "lollipop": 2,
+        "bubble gum": 1,
+        "laffy taffy": 3,
+        "milky way": 2,
+        "licorice": 1,
+        "chocolate bar": 1,
+        "nerds": 1,
+        "sour patch kids": 1
+    }
+
+    assert new_candy_data == expected_output
+
+    expected_output = {
+        "lollipop": 2,
+        "bubble gum": 1,
+        "laffy taffy": 3,
+        "milky way": 2,
+        "licorice": 1,
+        "chocolate bar": 1,
+        "nerds": 1,
+        "sour patch kids": 1
+    }
+
 # Add your own assertions to the test below
 def test_create_candy_data_structure_values():
 
@@ -33,13 +68,30 @@ def test_create_candy_data_structure_values():
     
     # Assert
     assert len(new_candy_data) == 8
-    '''
-    Add your assertions here!
-    '''
 
-'''
-5. 
-Starting with nominal cases, write tests for each of the functions 
-in the file tests/test_candy_data_structure.py then write tests to 
-handle edge cases.
-'''
+    def test_create_new_candy_data_structure():
+    # Arrange
+        friend_favorites = [
+        ["Sally", ["lollipop", "bubble gum", "laffy taffy"]],
+        ["Bob", ["milky way", "licorice", "lollipop"]],
+        ["Arlene", ["chocolate bar", "milky way", "laffy taffy"]],
+        ["Carlie", ["nerds", "sour patch kids", "laffy taffy"]]
+    ]
+    expected_output = {
+        "lollipop": ["Sally", "Bob"],
+        "bubble gum": ["Sally"],
+        "laffy taffy": ["Sally", "Arlene", "Carlie"],
+        "milky way": ["Bob", "Arlene"],
+        "licorice": ["Bob"],
+        "chocolate bar": ["Arlene"],
+        "nerds": ["Carlie"],
+        "sour patch kids": ["Carlie"]
+    }
+    
+    # Act
+    new_candy_data = create_new_candy_data_structure(friend_favorites)
+    
+    # Assert
+    assert new_candy_data == expected_output
+
+ 
